d6cac272ba02fceacd21c0b3a9ec3292c5a71da2
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2025-05-08 15:09:51 +0200

    add code to github

---
+++ Maven/MavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MavenApp.swift
+//  Maven
+//
+//  Created by Isabella Sulisufi on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
