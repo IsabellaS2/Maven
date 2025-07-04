e294af356e2a2b659bd813558e4da14e18c0ffc6
Isabella Sulisufi <isabella.sulisufi@isabella-sulisufi-l6t7lmc99p.lan>
2025-07-04 13:52:19 +0100

    Add models for score calculation and exmaple josn

---
+++ Maven/Models/ScoreCalculation/Accounts.swift
@@ -0,0 +1,54 @@
+
+//
+//  Accounts.swift
+//
+
+import Foundation
+
+struct Accounts: Codable {
+    var savingsAccounts: [SavingsAccount]
+    var creditCards: [CreditCard]
+    var loans: [Loan]
+    var bnplProviders: [BNPLUsageProvider]
+    var transactions: [Transaction]?
+    var creditScore: CreditScore
+
+    enum CodingKeys: String, CodingKey {
+        case savingsAccounts = "savings_accounts"
+        case creditCards = "credit_cards"
+        case loans, transactions
+        case bnplProviders = "bnpl_providers"
+        case creditScore = "credit_score"
+    }
+}
+
+struct CurrentAccount: Codable {
+    var accountId: String
+    var name: String
+    var balance: Balance
+    var sortCode: String
+    var accountNumber: String
+
+    enum CodingKeys: String, CodingKey {
+        case accountId, name, balance
+        case sortCode = "sort_code"
+        case accountNumber = "account_number"
+    }
+}
+
+struct SavingsAccount: Codable {
+    var accountId: String
+    var name: String
+    var balance: Balance
+
+    enum CodingKeys: String, CodingKey {
+        case accountId = "account_id"
+        case name, balance
+    }
+}
+
+struct Balance: Codable {
+    var current: Double
+    var currency: String
+}
+
---
+++ Maven/Models/ScoreCalculation/UserData.swift
@@ -0,0 +1,19 @@
+//
+//  UserData.swift
+//
+
+import Foundation
+
+// MARK: - Root
+struct ExplorerUserData: Codable {
+    var user: User
+    var behaviour: Behaviour
+    var knowledge: Knowledge
+}
+
+// MARK: - User
+struct User: Codable {
+    var id: String
+    var name: String
+    var email: String
+}
